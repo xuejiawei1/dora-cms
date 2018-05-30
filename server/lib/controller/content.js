428b6e769437b933f4cc9eb57b5284d9dc4825a9
xuejiawei1 <anotheryx@outlook.com>
2018-05-30 17:10:21 +0800

    fix 添加文章显示的问题

--- server/lib/controller/content.js
+++ server/lib/controller/content.js
@@ -294,7 +294,7 @@ class Content {
                 groupObj.stitle = groupObj.title;
                 groupObj.from = '3';
                 groupObj.uAuthor = !_.isEmpty(req.session.user) ? req.session.user._id : fields.author;
-                groupObj.state = false;
+                groupObj.state = fields.state;
                 groupObj.author = '';
             }
 
