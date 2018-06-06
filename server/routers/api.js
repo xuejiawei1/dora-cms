c6cff2c6d154b224effd91cb35dee5e5068b4484
xuejiawei1 <anotheryx@outlook.com>
2018-06-06 15:10:22 +0800

    fix

--- server/routers/api.js
+++ server/routers/api.js
@@ -111,7 +111,6 @@ router.get('/users/setNoticeRead', (req, res, next) => { req.query.user = !_.isE
 
 // 删除用户消息
 router.get('/users/delUserNotify', UserNotify.delUserNotify);
-
 // 获取用户参与话题
 router.get('/users/getUserReplies', (req, res, next) => { req.query.user = !_.isEmpty(req.session.user) ? req.session.user._id : req.query.author; next() }, Message.getMessages);
 
